3b42eee6b43fccd299bb3b797cdf576c93de8826
Manish Agarwal <manishagarwal7@gmail.com>
2015-09-04 22:45:24 -0500

    tried two way binding with collection inside existing object

--- js/controllers/helloController.js
+++ js/controllers/helloController.js
@@ -2,7 +2,7 @@
 
 
     $scope.employees = [
-        { firstName: "Phani", "lastName": "Kumar" },
+        { firstName: "Phani", "lastName": "Kumar", phones: [{ phone: "1221212122" }, {phone:"2332342342"}] },
         { firstName: "Raj", "lastName": "Kumar" },
         { firstName: "Kunal", "lastName": "Kumar" },
         { firstName: "Nitesh", "lastName": "Kumar" },
@@ -13,9 +13,19 @@
     $scope.insertEmp = function () {
 
         $scope.employees.push(
-            { firstName: $scope.firstEmployee, lastName: $scope.lastEmployee }
+            {
+                firstName: $scope.firstEmployee,
+                lastName: $scope.lastEmployee,
+                email: $scope.emailEmployee,
+                phones:
+            }
             );
     };
+
+    $scope.phones = [];
+
+    $scope.addphone = function () { $scope.phones.push($scope.phones.length); }
+
 
 
 
