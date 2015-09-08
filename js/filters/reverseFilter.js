848dfae5c3155706be39815a51621f88a53017fe
unknown <phaniwithu@gmail.com>
2015-09-08 08:46:16 +0530

    filter examples

---
+++ js/filters/reverseFilter.js
@@ -0,0 +1,14 @@
+﻿myApp.filter("reverse", function () {
+
+    return function (input) {
+
+        var result = "";
+
+        for (var i = input.length-1; i >= 0 ; i--) {
+            result += input[i];
+        }
+        return result;
+
+    };
+
+})
